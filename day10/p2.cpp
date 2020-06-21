401e100cf825c1315c8ed60113b4da3d4dcf3e66
Joshua Wong <joshwonghc@gmail.com>
2020-06-21 17:08:00 +0800

    Day 10 part 2

--- day10/p2.cpp
+++ day10/p2.cpp
@@ -8,10 +8,12 @@
 struct coord {
     long x;
     long y;
+    double vec_x;
+    double vec_y;
 
     bool operator <(const coord &b) const {
-        long magnitude_a = x * x + y * y;
-        long magnitude_b = b.x * b.x + b.y * b.y;
+        double magnitude_a = (vec_x * vec_x) + (vec_y * vec_y);
+        double magnitude_b = (b.vec_x * b.vec_x) + (b.vec_y * b.vec_y);
 
         if (magnitude_a > magnitude_b) {
             return false;
@@ -21,20 +23,10 @@ struct coord {
     }
 };
 
-double convertToDeg(double arctan_val);
-struct cmpByDeg {
-    bool operator()(const double& a, const double& b) const {
-        if (convertToDeg(a) > convertToDeg(b)) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-};
-
 std::vector<coord> getInput(std::string filepath);
 void parseLine(std::vector<coord> &vec, std::string s, int x_val);
-std::map<double, std::vector<coord>, cmpByDeg> getAsteroids(std::vector<coord> &coords, std::vector<coord>::iterator station);
+std::map<double, std::vector<coord>> getAsteroids(std::vector<coord> &coords, std::vector<coord>::iterator station);
+double convertToDeg(double arctan_val);
 bool magnitudeComparator(coord *a, coord *b);
 
 int main() {
@@ -43,11 +35,11 @@ int main() {
     int max_asteroids = 0;
     int idx = 0;
     std::vector<coord>::iterator it;
-    std::map<double, std::vector<coord>, cmpByDeg> asteroid_map;
+    std::map<double, std::vector<coord>> asteroid_map;
     coord station;
 
     for (it = asteroids.begin(); it != asteroids.end(); it++) {
-        std::map<double, std::vector<coord>, cmpByDeg> curr_map = getAsteroids(asteroids, it);
+        std::map<double, std::vector<coord>> curr_map = getAsteroids(asteroids, it);
         if (curr_map.size() > max_asteroids) {
             max_asteroids = curr_map.size();
             asteroid_map = curr_map;
@@ -55,40 +47,39 @@ int main() {
             idx = it - asteroids.begin();
         }
     }
-    
-    std::cout << asteroid_map.size() << "\n";
 
     // Remove the station from vector
     asteroids.erase(asteroids.begin() + idx);
     int asteroids_destroyed = 0;
-    std::map<double, std::vector<coord>, cmpByDeg>::iterator mapit;
+    std::map<double, std::vector<coord>>::iterator mapit;
 
-    // We just need to sort by the keys of the map
-    std::vector<double> keys;
     // Sort all the asteroids in each arctan by magnitude
     for (mapit = asteroid_map.begin(); mapit != asteroid_map.end(); mapit++) {
+        // Iterate through the asteroids and get the magnitude
+        for (it = mapit->second.begin(); it != mapit->second.end(); it++) {
+            it->vec_x = it->x - station.x;
+            it->vec_y = it->y - station.y;
+        }
+
         std::sort(mapit->second.begin(), mapit->second.end());
-        //keys.push_back(mapit->first);
-        //std::cout << mapit->first << "\n";
+
+        //for (it = mapit->second.begin(); it != mapit->second.end(); it++) {
+            //std::cout << it->vec_x * it->vec_x + it->vec_y * it->vec_y << "\n";
+        //}
+        //std::cout << "Done\n";
     }
-    //std::sort(keys.begin(), keys.end(), arctanComparator);
 
     while (true) {
         for (mapit = asteroid_map.begin(); mapit != asteroid_map.end(); mapit++) {
-            // Grab the vector, and delete the asteroid there, along with the vector if necessary
-            if (mapit->second.size() == 0) {
-                continue;
-            }
+            asteroids_destroyed++;
 
-            if (asteroids_destroyed == 199) {
+            if (asteroids_destroyed == 200) {
                 // stop and print next asteroid to destroy
                 coord target = *mapit->second.begin();
                 std::cout << target.x << " " << target.y << "\n";
                 return 0;
             }
 
-            mapit->second.erase(mapit->second.begin());
-            asteroids_destroyed++;
         }
     }
 }
@@ -118,10 +109,10 @@ void parseLine(std::vector<coord> &vec, std::string s, int x_val) {
     }
 }
 
-std::map<double, std::vector<coord>, cmpByDeg> getAsteroids(std::vector<coord> &coords, std::vector<coord>::iterator station) {
+std::map<double, std::vector<coord>> getAsteroids(std::vector<coord> &coords, std::vector<coord>::iterator station) {
     int asteroids_seen = 0;
     std::vector<coord>::iterator it;
-    std::map<double, std::vector<coord>, cmpByDeg> seen;
+    std::map<double, std::vector<coord>> seen;
 
     for (it = coords.begin(); it != coords.end(); it++) {
         if (it == station) {
@@ -130,20 +121,20 @@ std::map<double, std::vector<coord>, cmpByDeg> getAsteroids(std::vector<coord> &
 
         double vec_x = it->x - station->x;
         double vec_y = it->y - station->y;
-        double arctan_val = atan2(vec_y, vec_x);
-        if (seen.count(arctan_val) == 0) {
+        // NOTE: vec_x and vec_y switch places cos x is the y_value
+        // NOTE: -vec_x because index 0 is a the top. FML
+        double arctan_val = atan2(-vec_x, vec_y);
+        double deg = convertToDeg(arctan_val);
+        if (seen.count(deg) == 0) {
             std::vector<coord> values;
             values.push_back(*it);
-            seen.insert(std::pair<double, std::vector<coord>>(arctan_val, values));
+            seen.insert(std::pair<double, std::vector<coord>>(deg, values));
             asteroids_seen++;
         } else {
-            seen.find(arctan_val)->second.push_back(*it);
+            seen.find(deg)->second.push_back(*it);
         }
     }
 
-    if (seen.size() == 328) {
-        std::cout << asteroids_seen << "\n";
-    }
     return seen;
 }
 
@@ -152,7 +143,7 @@ double convertToDeg(double arctan_val) {
 
     // First quadrant
     if (deg >= 0 && deg <= 90) {
-        return abs(deg - 90);;
+        return 90 - deg;
     } else if (deg < 0) {
         // 2nd quadrant and 3rd quadrant
         return abs(deg) + 90;
